27f9e8bb13e839e7b31db1e99ec159a87b74c87a
neoteny <ss2316544@gmail.com>
2021-12-31 17:01:03 +0200

    added server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/Smirnov-O/noter/internal/config"
+	"github.com/Smirnov-O/noter/internal/server"
 	"github.com/Smirnov-O/noter/pkg/database"
 )
 
@@ -11,7 +20,7 @@ func main() {
 		panic(err)
 	}
 
-	_, err = database.NewConnection(database.ConnInfo{
+	db, err := database.NewConnection(database.ConnInfo{
 		Host:     cfg.Postgres.Host,
 		Port:     cfg.Postgres.Port,
 		Username: cfg.Postgres.Username,
@@ -20,6 +29,30 @@ func main() {
 		SSLMode:  cfg.Postgres.SSLMode,
 	})
 	if err != nil {
+		panic(err)
+	}
+
+	// Server
+	srv := server.NewServer(cfg, nil)
+	go func() {
+		if err := srv.Start(); !errors.Is(err, http.ErrServerClosed) {
+			panic(err)
+		}
+	}()
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	ctx, shutdown := context.WithTimeout(context.Background(), time.Second*5)
+	defer shutdown()
+
+	if err := srv.Stop(ctx); err != nil {
+		panic(err)
+	}
+
+	if err := db.Close(); err != nil {
 		panic(err)
 	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,32 @@
 package server
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/Smirnov-O/noter/internal/config"
+)
+
+type Server struct {
+	http *http.Server
+}
+
+func NewServer(cfg *config.Config, handler http.Handler) *Server {
+	return &Server{
+		http: &http.Server{
+			Addr:           ":" + cfg.HTTP.Port,
+			Handler:        handler,
+			ReadTimeout:    cfg.HTTP.ReadTimeout,
+			WriteTimeout:   cfg.HTTP.WriteTimeout,
+			MaxHeaderBytes: cfg.HTTP.MaxHeaderMegabytes << 20,
+		},
+	}
+}
+
+func (s *Server) Start() error {
+	return s.http.ListenAndServe()
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+	return s.http.Shutdown(ctx)
+}
