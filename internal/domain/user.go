86c4ca54ed20f15e26589b140fab8eac1cac3390
neoteny <ss2316544@gmail.com>
2021-12-31 18:11:14 +0200

    added user domain

---
+++ internal/domain/user.go
@@ -0,0 +1,20 @@
+package domain
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrUserAlreadyExists = errors.New("user already exeists")
+	ErrUserNotFound      = errors.New("user doesn't exists")
+)
+
+type User struct {
+	ID          int       `json:"id" db:"id"`
+	Name        string    `json:"name" db:"name"`
+	Email       string    `json:"email" db:"email"`
+	Password    string    `json:"password" db:"password"`
+	RegistredAt time.Time `json:"registred_at" db:"registred_at"`
+	LastVisitAt time.Time `json:"last_visit_at" db:"last_visit_at"`
+}
