83d4985839a4d2b29e9d79cc26b5a2790ab8ace1
flof-ik <ss2316544@gmail.com>
2022-02-17 22:06:19 +0200

    Added user sign-in testcase

--- tests/user_test.go
+++ tests/user_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"time"
 
 	"github.com/flof-ik/noter/internal/domain"
 	"github.com/gin-gonic/gin"
@@ -36,4 +37,27 @@ func (s *APITestSuite) TestUserSignUP() {
 	r.Equal(name, user.Name)
 	r.Equal(passwordHash, user.Password)
 	r.Equal(email, user.Email)
+}
+
+func (s *APITestSuite) TestUserSignIn() {
+	router := gin.New()
+	s.handler.Init(router.Group("/api"))
+	r := s.Require()
+
+	name, email, password := "Test User 2", "test2@test.com", "qwerty123"
+	signInData := fmt.Sprintf(`{"email":"%s","password":"%s"}`, email, password)
+	passwordHash, err := s.hasher.Hash(password)
+	s.NoError(err)
+
+	_, err = s.db.Exec("INSERT INTO users (name, email, password, registred_at, last_visit_at) VALUES ($1, $2, $3, $4, $5)",
+		name, email, passwordHash, time.Now(), time.Now())
+	r.NoError(err)
+
+	req, _ := http.NewRequest("POST", "/api/v1/auth/sign-in", bytes.NewBuffer([]byte(signInData)))
+	req.Header.Set("Content-type", "application/json")
+
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	s.Equal(http.StatusOK, resp.Result().StatusCode)
 }
